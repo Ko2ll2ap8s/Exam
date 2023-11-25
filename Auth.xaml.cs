28082b2d435bc3ed8115f42a0d2720dd7c4d6d73
Ko2ll2ap8s <116292559+Ko2ll2ap8s@users.noreply.github.com>
2023-11-25 12:53:22 +0300

    Add files via upload

---
+++ Auth.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : Window
+    {
+        public static student currUser;
+        public static organizator currOrganisatorUser;
+        public static string authCode;
+
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            MainWindow win = new MainWindow();
+            win.Show();
+        }
+
+        
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            try
+            {
+                var login = int.Parse(logBox.Text);
+                var password = passBox.Text;
+                currUser = RegistrMeetingEntities1.getContext().student.ToList().
+                    Where((u) => u.ID_student == login && u.пароль == password).Single();
+                if (currUser != null)
+                {
+                    authCode = random.Next(1111, 9999).ToString();
+                    MessageBox.Show(authCode);
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте корректность логина и пароля.");
+            }
+
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var code = codeBox.Text;
+            if(currUser != null)
+            {
+                MessageBox.Show("Успешно!", $"Привет, {currUser.ФИО}");
+            }
+            else if (currOrganisatorUser != null)
+            {
+                MessageBox.Show("Успешно!", $"Привет, {currOrganisatorUser.ФИО}");
+                OrganizerWindow.GetWindow(this).Show();
+            }
+
+        }
+    }
+}
---
+++ OrganizerWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для OrganizerWindow.xaml
+    /// </summary>
+    public partial class OrganizerWindow : Window
+    {
+        private organizator _currOrganisator = new organizator();
+        public OrganizerWindow(organizator selectedUser)
+        {
+            InitializeComponent();
+            if (_currOrganisator != null)
+                _currOrganisator = selectedUser;
+
+            DataContext = _currOrganisator;
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Profil win = new Profil();
+            win.Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
